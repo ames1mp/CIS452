b2dcaa60e0233c1533a8f4a39bbb04a399e9e7fe
Michael Ames <ames1mp@users.noreply.github.com>
2018-02-01 13:26:21 -0500

    Update prg.c

--- lab3/prg.c
+++ lab3/prg.c
@@ -2,45 +2,97 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
 
-#define SIZE 1024
+void signalHandler(int);
 
-void sigIntHandler(int);
-void sigHandlerUsr1(int);
-void sigHandlerUsr2(int);
 
 int main()
 {
-    signal(SIGINT, sigIntHandler);
-    signal(SIGUSR1, sigHandlerUsr1);
-    signal(SIGUSR2, sigHandlerUsr2);
+    pid_t parentPid;
+    parentPid = getpid();
 
-    printf("waiting . . . \n");
-    pause();
+    pid_t childPid = 0;
+
+    if( (childPid = fork()) < 0) {
+       perror("Fork failed.");
+        exit(1);
+    }
+    else if (!childPid) {
+        srand(time(NULL));
+        int randTime;
+        int randProcess;
+
+        while(1) {
+            fflush(stdout);
+
+            randTime = rand() % 4 + 1;
+            sleep(randTime);
+
+            randProcess = rand() % 2 + 1;
+
+            printf("%d", randTime);
+
+            if(randProcess == 1)
+                kill(parentPid, SIGUSR1);
+            else
+                kill(parentPid, SIGUSR2);
+        }
+
+    } else {
+
+        while(1) {
+
+        printf("waiting . . .");
+        fflush(stdout);
+
+        signal(SIGINT, signalHandler);
+        signal(SIGUSR1, signalHandler);
+        signal(SIGUSR2, signalHandler);
+
+        pause();
+
+        }
+
+    }
+
 
     return 0;
 }
 
-void sigIntHandler(int sigNum) {
+void signalHandler(int signalNumber) {
 
-    printf("^C received. That's it, I'm shutting you down . . .\n");
-    exit(0);
+        //CTRL-C interrupt signal SIGINT
+
+        if(signalNumber == SIGINT){
+
+            printf (" received an interrupt.\n");
+
+            // this is where shutdown code would be inserted
+            sleep (1);
+
+            printf ("outta here.\n");
+
+            exit(0);
+
+        }
+
+        //Check for SIGUSR1 interrupt
+
+        if(signalNumber == SIGUSR1) {
+
+         printf("RECEIVED SIGUSR1 SIGNAL\n");
+
+        }
+
+        //Check for SIGUSR2 interrupt
+
+        if(signalNumber == SIGUSR2) {
+
+         printf("RECEIVED SIGUSR2 SIGNAL\n");
+
+        }
 
 }
 
-void sigHandlerUsr1(int sigNum) {
-
-    printf("received a SIGUSR1 signal.");
-    fflush(stdout);
-    return;
-
-}
-
-void sigHandlerUsr2(int sigNum) {
-
-    printf("received a SIGUSR2 signal.");
-    fflush(stdout);
-    return;
-
-}
 
